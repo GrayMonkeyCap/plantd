a4bea9deaf0faa57ea17b8e669eb8eb663d3879e
Samita Kanojia <kanojiasamita2112@gmail.com>
2022-01-12 00:26:44 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,14 @@ import 'package:firstapp/pages/previous_reports.dart';
 import 'package:firstapp/pages/title.dart';
 import 'package:firstapp/pages/login.dart';
 import 'package:firstapp/pages/report.dart';
+import 'package:firstapp/pages/signup.dart';
 
 void main() => runApp(MaterialApp(initialRoute: '/', routes: {
       // ignore: prefer_const_constructors
       '/': (context) => title(),
       '/home': (context) => home(),
       '/login': (context) => login(),
+      '/signup': (context) => signup(),
       '/report': (context) => report(),
       '/previous_reports': (context) => previous_report(),
     }));
--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/gestures.dart';
 
-class login extends StatelessWidget {
+class signup extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,7 +11,9 @@ class login extends StatelessWidget {
           child: Column(
             children: <Widget>[
               Container(
-                margin: const EdgeInsets.fromLTRB(0, 30.0, 0, 50.0),
+                margin: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.width * 0.03,
+                    bottom: MediaQuery.of(context).size.width * 0.05),
                 child: RichText(
                     text: TextSpan(
                         style: const TextStyle(
@@ -22,28 +23,31 @@ class login extends StatelessWidget {
                             fontFamily: 'Salsa'),
                         children: [
                       const TextSpan(
-                          text: "Welcome to ",
+                          text: "SignUp Here ",
                           style: TextStyle(color: Colors.white)),
-                      TextSpan(
-                        text: "plantd",
-                        style: TextStyle(
-                          color: Colors.teal[900],
-                        ),
-                      )
                     ])),
               ),
-              Container(
-                margin: const EdgeInsets.only(bottom: 20.0),
-                child: const Text(
-                  "PLEASE LOGIN TO CONTINUE",
-                  style: TextStyle(
-                      color: Colors.white, letterSpacing: 1.25, fontSize: 14.0),
-                ),
+              Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(
+                        bottom: MediaQuery.of(context).size.width * 0.02,
+                        left: MediaQuery.of(context).size.width * 0.05),
+                    child: const Text(
+                      "IN JUST 1 STEP",
+                      style: TextStyle(
+                          color: Colors.white,
+                          letterSpacing: 1.25,
+                          fontSize: 14.0),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                ],
               ),
               Container(
                 // height: MediaQuery.of(context).size.height * 0.4,
                 width: MediaQuery.of(context).size.width * 0.9,
-                margin: const EdgeInsets.only(bottom: 30.0),
+                margin: const EdgeInsets.only(bottom: 30.0, top: 20.0),
 
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(10),
@@ -53,23 +57,21 @@ class login extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
                     Container(
-                      margin: EdgeInsets.only(top: 20.0, bottom: 20.0),
-                      child: TextButton.icon(
-                        onPressed: () {},
-                        icon: Image.asset(
-                          'assets/googlelogo.png',
-                          height: 20.0,
-                          width: 20.0,
-                        ),
-                        label: Text(
-                          "Sign in with Google ",
-                          style: TextStyle(color: Colors.grey),
+                      margin: EdgeInsets.only(bottom: 30.0, top: 30.0),
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: const TextField(
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.teal),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.teal),
+                          ),
+                          labelText: 'Enter Name',
+                          labelStyle: TextStyle(color: Colors.teal),
                         ),
                       ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(bottom: 20.0),
-                      child: const Text("OR"),
                     ),
                     Container(
                       margin: EdgeInsets.only(bottom: 30.0),
@@ -83,7 +85,42 @@ class login extends StatelessWidget {
                             borderSide:
                                 BorderSide(width: 1, color: Colors.teal),
                           ),
-                          labelText: 'Email/PhoneNo.',
+                          labelText: 'Enter EmailId',
+                          labelStyle: TextStyle(color: Colors.teal),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(bottom: 30.0),
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: const TextField(
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.teal),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.teal),
+                          ),
+                          labelText: 'Enter PhoneNo.',
+                          labelStyle: TextStyle(color: Colors.teal),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(bottom: 30.0),
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: const TextField(
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.teal),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.teal),
+                          ),
+                          suffixIcon: Icon(Icons.visibility),
+                          labelText: 'Enter Password',
                           labelStyle: TextStyle(color: Colors.teal),
                         ),
                       ),
@@ -100,7 +137,7 @@ class login extends StatelessWidget {
                                 BorderSide(width: 1, color: Colors.teal),
                           ),
                           suffixIcon: Icon(Icons.visibility),
-                          labelText: 'Password',
+                          labelText: 'Re-Enter Password',
                           labelStyle: TextStyle(color: Colors.teal),
                         ),
                       ),
@@ -109,10 +146,10 @@ class login extends StatelessWidget {
                       margin: const EdgeInsets.only(top: 30.0, bottom: 30.0),
                       child: TextButton(
                         onPressed: () {
-                          Navigator.pushReplacementNamed(context, '/home');
+                          Navigator.pushReplacementNamed(context, '/login');
                         },
                         child: const Text(
-                          'LOG IN',
+                          'SIGN UP',
                           style: TextStyle(
                               color: Colors.white,
                               fontSize: 12.0,
@@ -130,61 +167,6 @@ class login extends StatelessWidget {
                   ],
                 ),
               ),
-              Row(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(
-                        left: MediaQuery.of(context).size.width * 0.05),
-                    child: const Text(
-                      "DON'T HAVE ACCOUNT YET?",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 14.0,
-                          letterSpacing: 1.25),
-                      textAlign: TextAlign.left,
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Container(
-                      margin: EdgeInsets.only(
-                          left: MediaQuery.of(context).size.width * 0.05),
-                      child: GestureDetector(
-                        child: RichText(
-                          text: TextSpan(
-                            text: '',
-                            children: <TextSpan>[
-                              TextSpan(
-                                text: 'SIGNUP',
-                                style: TextStyle(
-                                    color: Colors.teal[900],
-                                    fontWeight: FontWeight.bold,
-                                    decoration: TextDecoration.underline,
-                                    fontSize: 14.0,
-                                    letterSpacing: 1.25),
-                                recognizer: TapGestureRecognizer()
-                                  ..onTap = () {
-                                    Navigator.pushReplacementNamed(
-                                        context, '/signup');
-                                  },
-                              ),
-                              TextSpan(
-                                text: '  HERE',
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 14.0,
-                                    letterSpacing: 1.25),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ))
-                ],
-              )
             ],
           )),
     );
